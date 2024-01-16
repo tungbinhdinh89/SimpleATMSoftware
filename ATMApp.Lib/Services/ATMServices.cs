7cb6a4b20fb0e92961bbd0b8b67efe1bb432a21c
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-16 22:34:42 +0700

    add validation ATM service

---
+++ ATMApp.Lib/Services/ATMServices.cs
@@ -0,0 +1,37 @@
+﻿using ATMApp.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.Lib.Services
+{
+    public class ATMServices
+    {
+        public List<ATM> GetDataFromJson(string path)
+        {
+            return [];
+        }
+
+        public bool ValidationATMCard(string cardNumber, string pin)
+        {
+            return true;
+        }
+
+        public decimal ShowBalance ()
+        { 
+            return 0;
+        }
+
+        public decimal Withdraw(int amount)
+        {
+            return 0;
+        }
+
+        public List<ATM> ShowLast5Transactions (int amount)
+        {
+            return new List<ATM> ();
+        }
+    }
+}
