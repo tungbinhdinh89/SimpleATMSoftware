3f710ec94772367ec01bdfbc544e90c99fd18cf7
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-16 22:33:51 +0700

    Add model

---
+++ ATMApp.Lib/Models/ATM.cs
@@ -0,0 +1,11 @@
+﻿namespace ATMApp.Lib.Models
+{
+    public class ATM
+    {
+        public string CardNumber { get; set; } = null!;
+        public string Pin { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+        public decimal Balance { get; set; }    
+
+    }
+}
