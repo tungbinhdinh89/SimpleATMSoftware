b51eaa2409d0d8dfee12d15dcbe6b3fafbd0d3b4
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-17 09:57:07 +0700

    add validation account service

--- ATMApp.Lib/Services/ATMServices.cs
+++ ATMApp.Lib/Services/ATMServices.cs
@@ -1,8 +1,11 @@
 ﻿using ATMApp.Lib.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ATMApp.Lib.Services
@@ -13,14 +16,35 @@ namespace ATMApp.Lib.Services
         {
             if (string.IsNullOrWhiteSpace(path))
             {
-                throw new ArgumentNullException("path not empty");
+                throw new ArgumentNullException(nameof(path), "Path should not be empty");
             }
-            return [];
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("File not found", path);
+            }
+
+            var jsonData = File.ReadAllText(path);
+
+            if (string.IsNullOrWhiteSpace(jsonData))
+            {
+                return new List<ATM>();
+            }
+
+            List<ATM> atm = JsonConvert.DeserializeObject<List<ATM>>(jsonData);
+
+            return atm ?? new List<ATM>();
         }
 
-        public bool ValidationATMCard(string cardNumber, string pin)
+        public bool ValidationATMCard(string cardNumber, string pin, List<ATM> atm)
         {
-            return true;
+            if (string.IsNullOrWhiteSpace(cardNumber) || string.IsNullOrWhiteSpace(pin) || atm == null)
+            {
+                return false; 
+            }
+            ATM matchingCard = atm.FirstOrDefault(x => x.CardNumber == cardNumber && x.Pin == pin);
+
+            return matchingCard != null;
         }
 
         public decimal ShowBalance ()
