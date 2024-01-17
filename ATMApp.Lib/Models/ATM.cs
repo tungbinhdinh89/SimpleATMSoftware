14e9becfbc6dccb85fcd47c52d0dfc035f48d8c8
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-17 08:44:15 +0700

    add transaction model

--- ATMApp.Lib/Models/ATM.cs
+++ ATMApp.Lib/Models/ATM.cs
@@ -1,11 +1,19 @@
 ﻿namespace ATMApp.Lib.Models
 {
+   
     public class ATM
     {
         public string CardNumber { get; set; } = null!;
         public string Pin { get; set; } = null!;
         public string FullName { get; set; } = null!;
-        public decimal Balance { get; set; }    
+        public decimal Balance { get; set; }   
+        public Transaction Transaction { get; set; } = new Transaction();
 
+    }
+
+    public class Transaction
+    {
+        DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
     }
 }
