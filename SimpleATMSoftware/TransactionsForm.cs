1f3f9447909596637b69c02b32b2193c371f4211
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-01-17 08:45:46 +0700

    add transaction form

--- SimpleATMSoftware/TransactionsForm.cs
+++ SimpleATMSoftware/TransactionsForm.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using ATMApp.Lib.Models;
+using ATMApp.Lib.Services;
 
 namespace SimpleATMSoftware
 {
     public partial class TransactionsForm : Form
     {
+        public ATMServices ATMServices { get; set; } = new ATMServices();
+
         public TransactionsForm()
         {
             InitializeComponent();
+        }
+
+        public void RenderToListView(List<Transaction> transactions)
+        {
+            foreach (Transaction transaction in transactions)
+            {
+                if (transaction == null)
+                {
+                    throw new ArgumentNullException("Not null");
+                }
+
+
+            }
         }
     }
 }
